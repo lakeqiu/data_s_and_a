63e63e004f75928af3016dfbc1107eb1ef3b9f54
lakeqiu <884506946@qq.com>
2020-03-07 11:00:01 +0800

    二叉搜索树删除节点

--- src/main/java/com/lake/_05_tree/BinarySearchTree.java
+++ src/main/java/com/lake/_05_tree/BinarySearchTree.java
@@ -3,6 +3,7 @@ package com.lake._05_tree;
 
 import com.lake._05_tree.print.BinaryTreeInfo;
 import com.sun.org.apache.bcel.internal.generic.NEW;
+import sun.reflect.generics.visitor.Visitor;
 
 import java.nio.Buffer;
 import java.util.*;
@@ -111,12 +112,99 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
         return ((Comparable<E>)e1).compareTo(e2);
     }
 
+    /**
+     * 删除二叉树中元素
+     * @param element
+     */
     public void remove(E element) {
+        // 找寻该值对应的节点
+        Node<E> node = findNode(element);
+        // 没有找到，说明二叉树中没有该元素
+        if (null == node) {
+            return;
+        }
+        // 找到了，删除该节点
+        remove(node);
+    }
 
+    /**
+     * 删除二叉树中节点
+     * @param node
+     */
+    private void remove(Node<E> node) {
+        // 1 判断该节点是否为度为2的节点
+        if (node.hasTwoChildren()) {
+            // 找到该节点的后继节点
+            Node<E> successor = successor(node);
+            // 用后继节点的值覆盖要删除节点的值
+            node.element = successor.element;
+            // 接下来结束删除后继节点了
+            node = successor;
+        }
+
+        // 2 删除后继节点
+        // node节点的度必然为0或1（这里就是来判断的）
+        // 2.1 找到node节点的代替节点
+        // 我们假设node的左子节点不为空的话，代替者等于左子节点，否则等于右子节点
+        Node<E> replacement = node.left != null ? node.left : node.right;
+
+        // 2.2 说明右子节点不为空，node节点度为1
+        if (replacement != null) {
+            // 更改代替节点的父节点
+            replacement.parent = node.parent;
+
+            // 更改父节点的left、right指向
+            if (node == node.parent.left) {
+                node.parent.left = replacement;
+            } else if (node == node.parent.right) {
+                node.parent.right = replacement;
+            } else {
+                // node节点是根节点
+                root = replacement;
+            }
+
+        } else if (node.parent == null) {
+            // 2.3 node节点为叶子节点并且为根节点
+            root = null;
+        } else {
+            // 2.4 node节点为叶子节点并且不是根节点
+
+            // 2.4.1 node节点为其父节点的左子节点
+            if (node == node.parent.left) {
+                node.parent.left = null;
+            }else {
+                // 2.4.2 node节点为其父节点的右子节点
+                node.parent.right = null;
+            }
+        }
+    }
+
+    /**
+     * 找寻该元素所在节点
+     * @param element
+     * @return
+     */
+    private Node<E> findNode(E element) {
+        Node<E> node = root;
+        while (node != null) {
+            // 1、比较当前节点值与所要寻找的节点的值的大小
+            int cmp = compare(element, node.element);
+
+            // 比当前节点值大，比较左子节点
+            if (cmp > 0) {
+                node = node.right;
+            } else if (cmp < 0){ // 比当前节点小，接下来比较右子节点
+                node = node.left;
+            } else { // 相等，找到了目标节点
+                return node;
+            }
+        }
+        // 到这里说明没有找到目标节点
+        return null;
     }
 
     public boolean contains(E element) {
-        return true;
+        return findNode(element) != null;
     }
 
     /**
@@ -124,7 +212,7 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
      */
     private void preorderTraversal(Node<E> e, Visitor<E> visitor) {
         // 终止条件
-        if (null == e || null == visitor) {
+        if (null == e || null == visitor || visitor.flag) {
             return;
         }
         // 1、访问根节点
@@ -151,7 +239,10 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
         while (!queue.isEmpty()) {
             // 3.1 出队一个节点并打印
             Node<E> node = queue.poll();
-            visitor.visit(node.element);
+            // 如果返回true，则结束遍历
+            if (visitor.visit(node.element)) {
+                return;
+            }
             // 3.2 入队node节点的左右节点
             if (node.left != null) {
                 queue.offer(node.left);
@@ -167,8 +258,9 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
     /**
      * 对遍历元素进行操作的接口
      */
-    public static interface Visitor<E> {
-        void visit(E element);
+    public static abstract class Visitor<E> {
+        boolean flag;
+        abstract boolean visit(E element);
     }
 
     /**
@@ -289,8 +381,67 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
         return true;
     }
 
+    /**
+     * 找寻节点的前驱节点
+     * @param node
+     * @return
+     */
+    private Node<E> predecessor(Node<E> node) {
+        if (null == node) {
+            return null;
+        }
+
+        // 1 如果节点的左子树不为空，则前驱节点在左子树中
+        if (node.left != null) {
+            Node<E> p = node.left;
+            // 1.1 一直找寻其右子树，直到尽头
+            while (p.right != null) {
+                p = p.right;
+            }
+
+            // 返回
+            return p;
+        }
+
+        // 2 如果节点的左子树为空并且父节点不为空，则这个节点是前驱节点的某一个父节点
+        // 2.1 一直找节点的父节点，直到这个节点在某个父节点的右子树上
+        while (node.parent != null && node == node.parent.left) {
+            node = node.parent;
+        }
+
+        // 3 能到这个，说明找到了前驱节点后前驱节点为空
+        // 这时，node.parent == null 或 node = node.parent.right
+        return node.parent;
+    }
+
+    /**
+     * 寻找后继节点
+     * @param node
+     * @return
+     */
+    private Node<E> successor(Node<E> node) {
+        if (null == node) {
+            return null;
+        }
+
+        if (node.right != null) {
+            Node<E> p = node.right;
+            while (p.left != null) {
+                p = p.left;
+            }
+            return p;
+        }
+
+        while (node.parent != null && node == node.parent.right) {
+            node = node.parent;
+        }
+
+        return node.parent;
+    }
+
     @Override
     public String toString() {
+
         return "BinarySearchTree{}";
     }
 
@@ -300,9 +451,17 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
         Node<E> right;
         Node<E> parent;
 
-        public Node(E element, Node<E> parent) {
+        Node(E element, Node<E> parent) {
             this.element = element;
             this.parent = parent;
+        }
+
+        public boolean isLeaf() {
+            return left == null && right == null;
+        }
+
+        public boolean hasTwoChildren() {
+            return left != null && right != null;
         }
     }
 
--- src/main/java/com/lake/_05_tree/Test5.java
+++ src/main/java/com/lake/_05_tree/Test5.java
@@ -2,21 +2,31 @@ package com.lake._05_tree;
 
 import com.lake._05_tree.print.BinaryTrees;
 
+import javax.swing.tree.TreeNode;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @author lakeqiu
  */
 public class Test5 {
     public static void main(String[] args) {
         BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        tree.add(22);
         for (int i = 0; i < 10; i++) {
             tree.add((int) (Math.random() * 100));
         }
 
+        tree.add(50);
+
         BinaryTrees.println(tree);
         tree.levelOrderTraversal(new BinarySearchTree.Visitor<Integer>() {
             @Override
-            public void visit(Integer element) {
+            public boolean visit(Integer element) {
                 System.out.print("_" + element + "_");
+                return false;
             }
         });
         System.out.println();
@@ -24,6 +34,51 @@ public class Test5 {
         System.out.println(tree.height2());
         System.out.println(tree.isComplete());
 
+        tree.remove(22);
+        BinaryTrees.println(tree);
+        System.out.println(tree.height());
         /*tree.toString();*/
+    }
+
+
+    class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (null == root) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        LinkedList<List<Integer>> list = new LinkedList();
+
+        TreeNode node = root;
+        queue.offer(node);
+
+        while (!queue.isEmpty()) {
+            List<Integer> integers = new LinkedList<>();
+
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                integers.add(queue.poll().val);
+                levelSize--;
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            list.addFirst(integers);
+            Stack stack = new Stack();
+        }
+
+        return list;
     }
 }
