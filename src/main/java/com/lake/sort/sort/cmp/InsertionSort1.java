dd988dd98d8dd3ae4cee09a376b4b5e2b2408832
lakeqiu <884506946@qq.com>
2020-03-23 10:02:01 +0800

    排序

---
+++ src/main/java/com/lake/sort/sort/cmp/InsertionSort1.java
@@ -0,0 +1,19 @@
+package com.lake.sort.sort.cmp;
+
+
+import com.lake.sort.sort.Sort;
+
+public class InsertionSort1<T extends Comparable<T>> extends Sort<T> {
+
+	@Override
+	protected void sort() {
+		for (int begin = 1; begin < array.length; begin++) {
+			int cur = begin;
+			while (cur > 0 && cmp(cur, cur - 1) < 0) {
+				swap(cur, cur - 1);
+				cur--;
+			}
+		}
+	}
+
+}
